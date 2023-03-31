8bc3a0560a7a9151bff803b64e33919252217e04
pikdonker <pikdonker@hotmail.com>
2023-03-31 21:22:52 +0200

    fix icons in intro

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -418,7 +418,7 @@ class EnhancementCalculatorComponent extends Component {
               <blockquote>
                 <p>Adapted from: <a href="https://ninjawithkillmoon.github.io/utilities/enhancementCalculator">The Arcane Library - Enhancement Calculator</a>. <a href="https://github.com/pikdonker/frosthaven-enhancement-calculator">Source</a></p>
                 <p>Each type of enhancement has a base cost. The cost might then be modified based on which ability is being enhanced.</p>
-                <p>Some enhancements do not fall neatly into the categories on the cost chart. When determining their base cost, treat damage traps as "<img alt="Attack Icon" src="../images/fh-attack-bw-icon.png" width={"12px"} /> + 1" enhancements (50 gold), treat healing traps as "<img alt="Heal Icon" src="../images/fh-heal-bw-icon.png" width={"12px"} /> + 1" enhancements (30 gold), and treat the movement of tokens and tiles as "<img alt="Move Icon" src="../images/fh-move-bw-icon.png" width={"12px"} /> + 1" enhancements (30 gold).</p>
+                <p>Some enhancements do not fall neatly into the categories on the cost chart. When determining their base cost, treat damage traps as "<img alt="Attack Icon" src="./images/fh-attack-bw-icon.png" width={"12px"} /> + 1" enhancements (50 gold), treat healing traps as "<img alt="Heal Icon" src="./images/fh-heal-bw-icon.png" width={"12px"} /> + 1" enhancements (30 gold), and treat the movement of tokens and tiles as "<img alt="Move Icon" src="./images/fh-move-bw-icon.png" width={"12px"} /> + 1" enhancements (30 gold).</p>
               </blockquote>
             </Col>
           </Row>
